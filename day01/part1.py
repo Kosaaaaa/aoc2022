89c62cdfb0504d4d7582fd17c6f5ee0098d55a1d
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2022-12-01 09:54:05 +0100

    add day01 part 1

---
+++ day01/part1.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    max_calories = 0
+    current_elf_calories = 0
+    for line in lines:
+        if not line:
+            if current_elf_calories > max_calories:
+                max_calories = current_elf_calories
+            current_elf_calories = 0
+        else:
+            current_elf_calories += int(line)
+
+    return max_calories
+
+
+INPUT_S = '''\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+'''
+EXPECTED = 24000
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
