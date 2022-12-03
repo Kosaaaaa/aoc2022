dcd957b179811dfb54fae1693229da6cec880d6a
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2022-12-03 16:16:31 +0100

    add day03 part 2

---
+++ day03/part2.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    priorities_sum = 0
+    items = iter(s.splitlines())
+
+    while True:
+        try:
+            s, = set(next(items)) & set(next(items)) & set(next(items))
+        except StopIteration:
+            break
+        else:
+            if s.islower():
+                priorities_sum += 1 + (ord(s) - ord('a'))
+            else:
+                priorities_sum += 27 + (ord(s) - ord('A'))
+
+    return priorities_sum
+
+
+INPUT_S = '''\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+'''
+EXPECTED = 70
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
