c6021b0d986e39da1abc1a0e923523be3e5d1ef0
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2022-12-08 09:02:52 +0100

    add day08 part 2

--- day08/part2.py
+++ day08/part2.py
@@ -11,21 +11,57 @@ INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
 def compute(s: str) -> int:
-    numbers = support.parse_numbers_split(s)
-    for n in numbers:
-        pass
+    coords = support.parse_coords_int(s)
 
-    lines = s.splitlines()
-    for line in lines:
-        pass
-    # TODO: implement solution here!
-    return 0
+    y_min, x_min = min(coords)
+    y_max, x_max = max(coords)
+
+    val = -1
+
+    def coords_compute(x: int, y: int) -> int:
+        n = coords[(y, x)]
+
+        up = 0
+        for cand_y in range(y - 1, y_min - 1, -1):
+            up += 1
+            if coords[(cand_y, x)] >= n:
+                break
+
+        down = 0
+        for cand_y in range(y + 1, y_max + 1):
+            down += 1
+            if coords[(cand_y, x)] >= n:
+                break
+
+        left = 0
+        for cand_x in range(x - 1, x_min - 1, -1):
+            left += 1
+            if coords[(y, cand_x)] >= n:
+                break
+
+        right = 0
+        for cand_x in range(x + 1, x_max + 1):
+            right += 1
+            if coords[(y, cand_x)] >= n:
+                break
+
+        return up * down * left * right
+
+    for y in range(y_min, y_max + 1):
+        for x in range(x_min, x_max + 1):
+            val = max(coords_compute(x, y), val)
+
+    return val
 
 
 INPUT_S = '''\
-
+30373
+25512
+65332
+33549
+35390
 '''
-EXPECTED = 1
+EXPECTED = 8
 
 
 @pytest.mark.parametrize(
